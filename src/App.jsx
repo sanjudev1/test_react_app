bb4659bd100eca47deec0ed3b53df852ed3eb313
sanjudev1 <sanjeevch.131@gmail.com>
2025-12-05 23:20:03 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 style={{color:"red"}}>Hello world</h1>
+        <img src="https://img.freepik.com/free-photo/waterfall-chae-son-national-park-lampang-thailand_554837-639.jpg?semt=ais_hybrid&w=740&q=80"/>
+      </div>
+    </>
+  )
+}
+
+export default App
